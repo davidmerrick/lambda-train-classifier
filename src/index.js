c2fa9114b3338f3370d0d60d2c414a8891d330c5
David Merrick <david.merrick@jivesoftware.com>
2017-05-28 18:26:15 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import Twitter from 'twitter'
+
+let client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+let params = {screen_name: process.env.SCREEN_NAME};
+
+client.get(`statuses/user_timeline`, params, (err, tweets, response) => {
+    return tweets;
+});
